85243b083f5b7c247ec1d7926f0c5214d277a249
voltum8 <mutalov02@mail.ru>
2021-05-25 03:27:17 +0500

    Initial commit

--- src/components/Content.js
+++ src/components/Content.js
@@ -9,7 +9,7 @@ function Content() {
             <img src={main_text_img} className="main_text_img" />
             <div className="main_btn_block">
                 <img src={balls_img} className="balls_img" />
-                <a href="#" className="main_btn"><img src={main_btn_img} /></a>
+                <a href="/download/RajBet.apk" className="main_btn" download><img src={main_btn_img} /></a>
             </div>
         </div>
     )
